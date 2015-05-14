84e76656a925ab31f3a88d3410397f41cee6462d
jerry <jerry1bean@hotmail.com>
2015-05-14 14:45:31 -0700

    fixed resolution for buttons

--- Assets/Scripts/Button/WheelButton.cs
+++ Assets/Scripts/Button/WheelButton.cs
@@ -6,12 +6,11 @@ using System.Collections;
 public class WheelButton : MonoBehaviour {
 	public PlayerController player;	
 	[SerializeField] private Button wheel; // assign in the editor
-	
+	private bool flipPressed = false;
 	void Start() { 
 		wheel = GetComponent<Button> ();
 		wheel.onClick.AddListener(() => { 
-			player.recordInputsHelper("Space");
+			flipPressed = true;
 		});
-	}
-
-}
+	}	
+}	
