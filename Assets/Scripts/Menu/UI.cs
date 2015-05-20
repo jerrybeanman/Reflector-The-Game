fd3e7214e64d35ee52f0de3c3b5bb6f79e2aa3ef
She Jong <kkagill@gmail.com>
2015-05-20 15:30:36 -0700

    fixing script for login

--- Assets/Scripts/Menu/UI.cs
+++ Assets/Scripts/Menu/UI.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+//using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
 
 public class UI : MonoBehaviour {
 
 	AudioSource sound;
 
 	void Awake() {
-		sound = GetComponent<AudioSource>();
+		sound = GetComponent<AudioSource>();	
+		//PlayGamesPlatform.Activate();	
 	}
 	
 	public void DisableBoolInAnimator(Animator anim) {
@@ -20,6 +23,24 @@ public class UI : MonoBehaviour {
 	
 	public void NavigateTo(int scene) {
 		Application.LoadLevel (scene);
+	}
 
+	public void OnGUI() {
+		if(GUILayout.Button("login")) {
+			//login
+			Social.localUser.Authenticate((bool success) => {
+				// handle success or failure
+				if (success) {
+					Debug.Log("success");
+				} else {
+					Debug.Log("failed");
+				}
+			});
+		}
+
+		if(GUILayout.Button("Achievement")) {
+			// show achievements UI
+			Social.ShowAchievementsUI();
+		}
 	}
 }
